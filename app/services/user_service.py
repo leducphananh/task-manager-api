7eb3abd8b378c98d581dda9b092938f57aaa83eb
AnhLDP <leducphananh@gmail.com>
2026-07-26 22:58:01 +0700

    feat: implement user service registration logic and password hashing utilities using bcrypt

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,6 +1,7 @@
 from app.repositories.user_repository import UserRepository
 from app.schemas.user import UserCreate
 from app.models.user import User
+from app.core.security import hash_password
 
 
 class UserService:
@@ -13,10 +14,11 @@ class UserService:
             from app.exceptions import EmailAlreadyExistsException
             raise EmailAlreadyExistsException()
 
-        # hashed_password = hash_password(user.password)
+        hashed_password = hash_password(user.password)
+
         new_user = User(
             name=user.name,
             email=user.email,
-            password_hash=user.password
+            password_hash=hashed_password
         )
         return self.repository.create(new_user)
